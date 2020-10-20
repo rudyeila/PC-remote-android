159310433942aaa6683eb82f01900f97edd85b20
Rudy Ailabouni <eilabouni.rudy@gmail.com>
2020-10-20 20:35:50 +0200

    added listeners to main activity

--- app/src/main/java/com/example/pc_remote_android/MainActivity.kt
+++ app/src/main/java/com/example/pc_remote_android/MainActivity.kt
@@ -1,17 +1,23 @@
 package com.example.pc_remote_android
 
-//
-
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
+import android.view.MotionEvent
+import android.view.View
+import android.widget.MediaController
 import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MotionEventCompat
 import androidx.viewpager.widget.ViewPager
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.Volley
+import com.example.pc_remote_android.controllers.MouseController
 import com.example.pc_remote_android.models.VolumeViewModel
 import com.example.pc_remote_android.networking.HTTPClient
 import com.example.pc_remote_android.ui.main.SectionsPagerAdapter
@@ -22,14 +28,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.UnsupportedEncodingException
+import kotlin.math.round
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     private val baseUrl = "http://192.168.178.23/"
     private lateinit var queue: RequestQueue
     private lateinit var httpClient: HTTPClient
     private lateinit var volumeViewModel: VolumeViewModel
+    private lateinit var mouseController: MouseController
 
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -48,6 +58,9 @@ class MainActivity : AppCompatActivity() {
 
         // Set up Volume ViewModel
         volumeViewModel = VolumeViewModel(httpClient, CustomErrorListener())
+
+        // Set up mouse controller
+        mouseController = MouseController(httpClient, CustomErrorListener())
 
         // Add live data observers
         volumeViewModel.getCurrentVolume().observe(this, { newVolume: Int ->
@@ -69,25 +82,79 @@ class MainActivity : AppCompatActivity() {
 
         muteSwitch.setOnCheckedChangeListener { _, isChecked -> volumeViewModel.setMute(isChecked) }
 
+        textViewError.setOnClickListener() { view: View ->
+            (view as TextView).text = ""
+            Log.d("View On Click" , "View Clicked!")
+        }
+
         seekBarVolume.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            private var isTracking: Boolean = false
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-//                textViewVolume.text = progress.toString()
+//                if (seekBar != null) {
+//                    volumeViewModel.setVolume(progress)
+//                    Thread.sleep(25);
+//                }
+                return
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                isTracking = true
                 return
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                isTracking = false
                 if (seekBar != null) {
                     volumeViewModel.setVolume(seekBar.progress)
                 }
                 return
             }
         })
+
+        mouseArea.setOnTouchListener { v, event ->
+            val action: Int = MotionEventCompat.getActionMasked(event)
+            val DEBUG_TAG = "MouseAreaTouchListener"
+            when (action) {
+                MotionEvent.ACTION_DOWN -> {
+                    Log.d(DEBUG_TAG, "Action was DOWN")
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    Log.d(DEBUG_TAG, "Action was MOVE")
+//                  Log.d(DEBUG_TAG, "OnTouch (x,y): (" + event.getX() + ", "  + event.getY())
+                    if (event.historySize > 0) {
+                        val lastX =
+                            event.getHistoricalAxisValue(MotionEvent.AXIS_X, 0)
+                        val lastY =
+                            event.getHistoricalAxisValue(MotionEvent.AXIS_Y, 0)
+                        val newX = event.getX()
+                        val newY = event.getY()
+                        val deltaX = (newX - lastX).roundToInt();
+                        val deltaY = (newY - lastY).roundToInt()
+                        Log.d(DEBUG_TAG, "LastX = ${lastX} and lastY = ${lastY} ")
+                        Log.d(DEBUG_TAG, "NewX = ${newX} and newY = ${newY} ")
+                        Log.d(
+                            DEBUG_TAG,
+                            "Delta (x,y): (" + deltaX + ", " + deltaY + ")"
+                        )
+                        mouseController.moveMouse(deltaX, deltaY);
+                        Thread.sleep(30);
+                    }
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    Log.d(DEBUG_TAG, "Action was UP")
+                    true
+                }
+                MotionEvent.ACTION_CANCEL -> {
+                    Log.d(DEBUG_TAG, "Action was CANCEL")
+                    true
+                }
+                MotionEvent.ACTION_OUTSIDE -> {
+                    Log.d(DEBUG_TAG, "Movement occurred outside bounds of current screen element")
+                    true
+                }
+                else -> super.onTouchEvent(event)
+            }
+
+        }
 
     }
 
@@ -138,6 +205,7 @@ class MainActivity : AppCompatActivity() {
                 val message = jsonMessage.getString("message")
                 Log.e("getErrorResponse", message)
                 textViewError.text = message
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT)
             } catch (e: JSONException) {
                 Log.e("getErrorResponse", "JSONException".plus(e.toString()))
                 textViewError.text = e.toString()
